f3c9dafe7286eda7790bd739d01a06eeaf2eebc6
Min-Huang <minh.huang96@gmail.com>
2021-10-27 23:25:15 -0700

    button 3-6

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -6,11 +6,16 @@ function Buttons() {
       style={{
         margin: "30px",
         display: "flex",
+        flexWrap: "wrap",
         justifyContent: "space-evenly",
       }}
     >
       <div className="button button1">Button 1</div>
       <div className="button button2">Button 2</div>
+      <div className="button button3">Button 3</div>
+      <div className="button button4">Button 4</div>
+      <div className="button button5">Button 5</div>
+      <div className="button button6">Button 6</div>
     </div>
   );
 }
