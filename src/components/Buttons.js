bb6303e55aa82f1049ae64e3d9ce566015627f78
Min-Huang <minh.huang96@gmail.com>
2021-11-07 10:35:01 -0800

    button 14

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -16,6 +16,19 @@ function Buttons() {
       <div className="button button4">Button 4</div>
       <div className="button button5">Button 5</div>
       <div className="button button6">Button 6</div>
+      <div className="button button7">Button 7</div>
+      <div className="button button8">Button 8</div>
+      <button className="button button9">Button 9</button>
+      <div className="button button10">Button 10</div>
+      <div className="button button11">Button 11</div>
+      <div className="button button12">
+        <span></span>Button 12
+      </div>
+      <div className="button button13">Button 13</div>
+      <div className="button button14">
+        <span>Button 14</span>
+      </div>
+      <div className="button button15">Button 15</div>
     </div>
   );
 }
